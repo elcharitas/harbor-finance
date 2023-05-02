141253400234179615ef3fed43759e6de92025f2
elcharitas <jonathanirhodia@gmail.com>
2023-05-02 01:41:39 +0100

    chore: create provider and hooks

---
+++ src/providers/app/provider.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { SaasProvider } from "@saas-ui/react";
+import { FC } from "react";
+import theme from "src/theme";
+
+const AppProvider: FC<{ children: React.ReactNode }> = ({ children }) => (
+  <SaasProvider theme={theme}>{children}</SaasProvider>
+);
+
+export default AppProvider;
