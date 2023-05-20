8b9beaf6352edd0a7ca1449ce51395d384b32466
elcharitas <jonathanirhodia@gmail.com>
2023-05-20 15:32:43 +0100

    chore: create TDai token

---
+++ contracts/tokens/TDai.sol
@@ -0,0 +1,11 @@
+ 
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+contract TDai is ERC20, ERC20Permit {
+    constructor() ERC20("TDai", "TDAI") ERC20Permit("TDai") {
+        _mint(msg.sender, 1000000000000000000000000);
+    }
+}
