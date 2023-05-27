0953c32a87fe568564409abeff2cbfcd0210c253
elcharitas <jonathanirhodia@gmail.com>
2023-05-27 10:16:55 +0100

    chore: add contracts config

---
+++ src/configs/contracts.ts
@@ -0,0 +1,5 @@
+const CONTRACTS_CONFIG: Record<string, `0x${string}`> = {
+  SAVINGS_GOAL_FACTORY: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+};
+
+export default CONTRACTS_CONFIG;
--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,7 +1,9 @@
 import APP_CONFIG from "./app";
+import CONTRACTS_CONFIG from "./contracts";
 
 const CONFIG = {
   APP: APP_CONFIG,
+  CONTRACTS: CONTRACTS_CONFIG,
 };
 
 export default CONFIG;
