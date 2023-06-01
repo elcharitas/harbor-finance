1283b6da63d9a809ed96ac4e50db83427b4fc38f
elcharitas <jonathanirhodia@gmail.com>
2023-06-01 13:59:16 +0100

    fix: contract imports

--- contracts/savings/SavingsGoal.sol
+++ contracts/savings/SavingsGoal.sol
@@ -3,7 +3,6 @@ pragma solidity 0.8.19;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Permit.sol";
 
 /**
  * SavingsGoal contract
